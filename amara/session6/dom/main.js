faf6401ee84b2f388a3c2a99636aaef8693b4ee7
TOURE881 <76970680+TOURE881@users.noreply.github.com>
2021-02-18 18:14:25 +0100

    Update main.js

--- amara/session6/dom/main.js
+++ amara/session6/dom/main.js
@@ -5,14 +5,15 @@ console.log("Vous êtes dans l'exercice 1 sur le DOM");
 // ------ TODO 1 ------
 // Créez une variable récupérant l'élément button ayant l'id "myButton" du HTML.
 
-
+document.getElementsByTagName('button');
 
 
 // ------ TODO 2 ------
 // Modifier le texte de l'élément button grâce à sa propriété innerHTML.
 
-
-
+var content = document.getElementsByClassName("content");
+var monH1 = content[0];
+monH1.innerHTML = "mon nouvaeu text";
 
 // ------ TODO 3 ------
 // Créez une variable récupérant tous les éléments de type paragraphe.
